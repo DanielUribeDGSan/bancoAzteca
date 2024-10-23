26f42fb8366e4fad3a9bc741653770f11e2e9442
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2024-10-23 14:18:25 -0600

    feat: cambios v2

--- src/screens/agenda/Agenda.tsx
+++ src/screens/agenda/Agenda.tsx
@@ -1,8 +1,19 @@
+import { MenuTop } from "../../components/menu/MenuTop";
+
 export const Agenda = () => {
   return (
-    <img
-      src="/assets/img/agenda/Agenda_2.png"
-      className="img-fluid agenda-image w-100 h-100"
-    />
+    <>
+      <MenuTop
+        styleMenu={{
+          top: "0% ",
+          left: "0% ",
+        }}
+      />
+
+      <img
+        src="/assets/img/agenda/Agenda_2.png"
+        className="img-fluid agenda-image w-100 h-100 pt-20"
+      />
+    </>
   );
 };
