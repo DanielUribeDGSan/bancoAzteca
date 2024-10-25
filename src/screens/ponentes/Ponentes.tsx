55dbf3e4627440b7eb1a81e4e9d930898001db86
DanielUribeDGSan <daniel.uribe.garcia07@gmail.com>
2024-10-24 18:24:14 -0600

    feat: new changes

--- src/screens/ponentes/Ponentes.tsx
+++ src/screens/ponentes/Ponentes.tsx
@@ -5,7 +5,6 @@ import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardActions from "@mui/material/CardActions";
-import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import SettingsVoiceIcon from "@mui/icons-material/SettingsVoice";
 import { useState } from "react";
